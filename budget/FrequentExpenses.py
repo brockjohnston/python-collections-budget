5682860086c0431ecfd13428b06d032edd31ffb2
brockjohnston <90159957+brockjohnston@users.noreply.github.com>
2022-05-20 12:51:02 +1000

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -16,5 +16,5 @@ categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
 ax.bar(categories, count)
-ax.set_title('# of Purchases by Categories')
+ax.set_title('# of Purchases by Category')
 plt.show()
